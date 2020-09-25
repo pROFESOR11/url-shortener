9a30ea70144e39a48c0bc57f2803310e2214b2a3
pROFESOR11 <ali_cimen_11@hotmail.com>
2020-09-25 19:09:48 +0300

    add toastr

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import { TextInput } from "./components/TextInput";
 import { Container } from "./components/Container";
 import { Button } from "semantic-ui-react";
+import { useToasts } from "react-toast-notifications";
 
 const schema = yup.object().shape({
   url: yup.string().trim().url().required(),
@@ -17,10 +18,14 @@ function App() {
   const [isValid, setisValid] = React.useState(false);
   const [isCopied, setisCopied] = React.useState(false);
 
+  const { addToast } = useToasts();
+
+  // handle url input value change
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     seturl(event.target.value);
   };
 
+  // generate short url mutation
   const [generateSlug, { isLoading }] = useMutation(() =>
     axios.post(`${process.env.REACT_APP_SERVER_URI}/add-shortUrl`, {
       url,
@@ -49,53 +54,91 @@ function App() {
           generateResponse.data.shortUrl.slug;
         setshortUrl(slug);
       }
+      addToast("Successfully generated.", {
+        appearance: "success",
+        autoDismiss: true,
+      });
     } catch (err) {
-      console.log("err", err);
+      // notify user
+      addToast("Something went wrong!", {
+        appearance: "error",
+        autoDismiss: true,
+      });
     }
   };
 
+  // copy to clipboard action
+  // notify if any error (including permission errors) occures
   function handleCopyToClipboard() {
     try {
       navigator.clipboard.writeText(shortUrl);
       setisCopied(true);
     } catch (error) {
       setisCopied(false);
-      window.alert(error.message || "Something went wrong!");
+      addToast(
+        "Couldn't copy to clipboard.\nWe need your permission to do that.",
+        {
+          appearance: "error",
+          autoDismiss: true,
+        }
+      );
     }
   }
 
+  // get clipboard text if available
   async function getClipboard() {
     try {
       const text = await navigator.clipboard.readText();
       return text;
     } catch (error) {
-      return;
+      addToast(
+        "Couldn't use clipboard.\nWe need your permission for a better experience.",
+        {
+          appearance: "warning",
+          autoDismiss: true,
+        }
+      );
     }
   }
 
-  const setClipboardStatus = React.useCallback(async () => {
+  // get clipboard text and set as url input text value
+  // notify if any error occures
+  const setClipboardStatus = async () => {
     try {
       const text = await getClipboard();
       if (text) {
         seturl(text);
       }
     } catch (err) {
-      window.alert(err.message);
+      addToast("Something went wrong!", {
+        appearance: "error",
+        autoDismiss: true,
+      });
     }
-  }, []);
+  };
 
+  // open url in a new window
+  // notify if any error occurs
   function handleGoToUrl() {
-    window.open(shortUrl, "_blank");
+    try {
+      window.open(shortUrl, "_blank");
+    } catch (err) {
+      addToast("Couldn't open url.", {
+        appearance: "error",
+        autoDismiss: true,
+      });
+    }
   }
 
   // if user opens this app with data in clipboard
   // then automatically set url text input to clipboard data
   // which will improve UX
+  // notify if any error occurs
   React.useEffect(() => {
     (async function () {
       await setClipboardStatus();
     })();
-  }, [setClipboardStatus]);
+  }, []);
 
   // listen to url changes
   // if validated set isValid flag to true
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "semantic-ui-css/semantic.min.css";
+import { ToastProvider } from "react-toast-notifications";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ToastProvider>
+      <App />
+    </ToastProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
